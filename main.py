cdc87527736bd28b8f453ec28f7dc8bf155792e4
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-06-13 01:49:28 +0300

    Add an options file where I'll be developing options independently  (#27)
    * chore: Seperate the workload into different files
    
    * add options to dockerfile
    
    * return to `owner@owner`
    
    * update the wordings
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by yapf
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>

--- main.py
+++ main.py
@@ -9,46 +9,9 @@ from collections import defaultdict
 import requests
 from github import Github
 
-# Include variables in the readme so that every commit can be "unique"
-# They can be expanded as your wish
+import options
 
-# Start the variable script
-input_string = os.getenv("INPUT_COMMIT_MESSAGE")
-now = datetime.datetime.now()
-current_date = now.strftime("%a %d %b %Y %H:%M:%S")
-
-full_year = now.strftime("%d %b %Y")
-full_time = now.strftime("%a %H:%M:%S")
-year = now.year
-month_no = now.month
-month_name = now.strftime("%B")
-day_no = now.day
-day_name = now.strftime("%A")
-minute = now.minute
-hour = now.hour
-my_username = os.getenv("INPUT_USERNAME")
-
-# Define a dictionary with placeholders and their corresponding values
-placeholders = {
-    "date": current_date,
-    "year": year,
-    "month_name": month_name,
-    "month_no": month_no,
-    "day_name": day_name,
-    "day_no": day_no,
-    "minute": minute,
-    "hour": hour,
-    "username": my_username,
-    "full_year": full_year,
-    "full_time": full_time,
-}
-
-# Iterate over the placeholders and replace them in the input string
-u_message = input_string
-for placeholder, value in placeholders.items():
-    u_message = u_message.replace(f"``{placeholder}``", str(value))
-
-# End the variable script
+c_message = options.generate_commit_message(os.getenv("INPUT_COMMIT_MESSAGE"))
 
 START_COMMENT = "<!--START_SECTION:top_language-->"
 END_COMMENT = "<!--END_SECTION:top_language-->"
@@ -215,11 +178,10 @@ if __name__ == "__main__":
     old_readme = str(base64.b64decode(content.content), "utf-8")
     new_readme = re.sub(listReg, f"{START_COMMENT}\n{stat_str}\n{END_COMMENT}",
                         old_readme)
-    if new_readme != old_readme:
-        repo.update_file(
-            branch="master",
-            path=content.path,
-            sha=content.sha,
-            message=u_message,
-            content=new_readme,
-        )
+    repo.update_file(
+        branch="master",
+        path=content.path,
+        sha=content.sha,
+        message=c_message,
+        content=new_readme,
+    )
---
+++ options.py
@@ -0,0 +1,38 @@
+import datetime
+import os
+
+
+def generate_commit_message(input_string):
+    now = datetime.datetime.now()
+    current_date = now.strftime("%a %d %b %Y %H:%M:%S")
+
+    full_year = now.strftime("%a %d %b %Y")
+    full_time = now.strftime("%H:%M:%S")
+    year = now.year
+    month_no = now.month
+    month_name = now.strftime("%B")
+    day_no = now.day
+    day_name = now.strftime("%A")
+    minute = now.minute
+    hour = now.hour
+    my_username = os.getenv("INPUT_USERNAME")
+
+    placeholders = {
+        "date": current_date,
+        "year": year,
+        "month_name": month_name,
+        "month_no": month_no,
+        "day_name": day_name,
+        "day_no": day_no,
+        "minute": minute,
+        "hour": hour,
+        "username": my_username,
+        "full_year": full_year,
+        "full_time": full_time,
+    }
+
+    u_message = input_string
+    for placeholder, value in placeholders.items():
+        u_message = u_message.replace(f"``{placeholder}``", str(value))
+
+    return u_message
